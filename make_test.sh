1b2c87696aba3367c1cbfd9787c680f06e9346e2
Will Roberts <will.roberts@rz.hu-berlin.de>
2013-12-06 21:32:00 +0100

    add some stuff to make_test.sh

--- make_test.sh
+++ make_test.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+# count the lines in a.txt (winners of FIFA World Cup)
 cat a.txt | ./count > a-cnt.txt
+# count the lines in b.txt (runners-up of FIFA World Cup)
 cat b.txt | ./count > b-cnt.txt
+# reverse the lines of b-cnt.txt and save this as c-cnt.txt
+tail -r b-cnt.txt > c-cnt.txt
+
+# for testing, e.g.:
+# ./addcount - b-cnt.txt
+# ./addcount a-cnt.txt b-cnt.txt
+# ./addcount a-cnt.txt c-cnt.txt
