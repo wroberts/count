9d534132fba63c808db2b321fe0e19d126a203b5
Will Roberts <will.roberts@rz.hu-berlin.de>
2013-12-06 19:23:50 +0100

    count.cpp: add -f option to sort output by frequency

--- count.cpp
+++ count.cpp
@@ -34,8 +34,9 @@ void
 printHelp()
 {
     cout << "Counts unique lines on standard input, outputting the counts after all" << endl;
-    cout << "input is read.  The output of the program can be piped through sort -nr " << endl;
-    cout << "to sort it in order of descending frequency." << endl;
+    cout << "input is read.  Output is sorted in alphabetical order of lines.  The" << endl;
+    cout << "output of the program can be sort it in order of descending frequency" << endl;
+    cout << "using the -f option or by piping through sort -nr." << endl;
     cout << endl;
     cout << "Syntax:" << endl;
     cout << endl;
@@ -45,21 +46,41 @@ printHelp()
     cout << endl;
     cout << "   -e      always count the last line of the input, even if it is" << endl;
     cout << "           empty" << endl;
+    cout << "           -f      sort output in order of descending frequency" << endl;
     cout << "   -?      display this help message and exit" << endl;
+}
+
+template<typename A, typename B>
+pair<B,A> flip_pair(const pair<A,B> &p)
+{
+    return pair<B,A>(p.second, p.first);
+}
+
+template<typename A, typename B>
+map<B,A> flip_map(const map<A,B> &src)
+{
+    map<B,A> dst;
+    transform(src.begin(), src.end(), inserter(dst, dst.begin()),
+              flip_pair<A,B>);
+    return dst;
 }
 
 int
 main ( int    argc,
        char **argv )
 {
-    bool       fIncludeLastLine = false;
+    bool       fIncludeLastLine    = false;
+    bool       fSortDecreasingFreq = false;
     int        c;
-    while ((c = getopt(argc, argv, "e?")) != -1)
+    while ((c = getopt(argc, argv, "ef?")) != -1)
     {
         switch(c)
         {
         case 'e':
             fIncludeLastLine = true;
+            break;
+        case 'f':
+            fSortDecreasingFreq = true;
             break;
         case '?':
             printHelp();
@@ -88,10 +109,23 @@ main ( int    argc,
     //    nWidth++;
     //    nNumLines /= 10;
     //}
-    for ( map<string,int>::iterator iterator = LineDict.begin();
-          iterator != LineDict.end(); iterator++ )
+    if (fSortDecreasingFreq)
     {
-        cout << iterator->second << "\t" << iterator->first << endl;
+        map<int,string> FlippedLineDict = flip_map(LineDict);
+        for ( map<int,string>::reverse_iterator iterator =
+                  FlippedLineDict.rbegin();
+              iterator != FlippedLineDict.rend(); iterator++ )
+        {
+            cout << iterator->first << "\t" << iterator->second << endl;
+        }
+    }
+    else
+    {
+        for ( map<string,int>::iterator iterator = LineDict.begin();
+              iterator != LineDict.end(); iterator++ )
+        {
+            cout << iterator->second << "\t" << iterator->first << endl;
+        }
     }
     return 0;
 }
